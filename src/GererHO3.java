ead2e067c5a0241ae909929485495ea6a20eb575
lineeeeeeeeeeeeee <line.peronnet@gmail.com>
2025-12-02 20:48:02 +0100

    Create GererHO3.java

---
+++ src/GererHO3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GererHO3 {
+    private Scanner scanner;
+
+    public GererHO3(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void afficherMenu() {
+        System.out.println("\nFonctionnalité HO3 (Mixte) - À implémenter");
+        System.out.println("Cette hypothèse mixte sera développée dans une future version.");
+
+        System.out.print("\nAppuyez sur Entrée pour continuer...");
+        scanner.nextLine();
+    }
+}
