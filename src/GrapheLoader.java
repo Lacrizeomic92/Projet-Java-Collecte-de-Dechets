a6cafd93e3166bf49ecce82245895f18045073df
lineeeeeeeeeeeeee <line.peronnet@gmail.com>
2025-12-04 12:37:36 +0100

    Create GrapheLoader.java

---
+++ src/GrapheLoader.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class GrapheLoader {
+
+    public static Graphe chargerDepuisFichier(String path) {
+        Graphe graphe = new Graphe();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String ligne;
+            boolean lectureNodes = false;
+            boolean lectureEdges = false;
+
+            while ((ligne = br.readLine()) != null) {
+
+                ligne = ligne.trim();
+
+                if (ligne.equals("NODES")) {
+                    lectureNodes = true;
+                    lectureEdges = false;
+                    continue;
+                }
+                if (ligne.equals("EDGES")) {
+                    lectureEdges = true;
+                    lectureNodes = false;
+                    continue;
+                }
+
+                if (ligne.isEmpty()) continue;
+
+                if (lectureNodes) {
+                    String[] parts = ligne.split(" ", 2);
+                    graphe.nodes.add(new Graphe.Node(parts[0], parts[1]));
+                }
+
+                if (lectureEdges) {
+                    String[] parts = ligne.split(" ");
+                    graphe.edges.add(new Graphe.Edge(
+                            parts[0], parts[1],
+                            Integer.parseInt(parts[2]),
+                            parts[3]
+                    ));
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return graphe;
+    }
+}
